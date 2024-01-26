7d518e24faeee40fb025c63473dff1379cc8289f
Anne Sannella <27774757+Annesannella1@users.noreply.github.com>
2024-01-26 17:11:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,44 @@ console.log(canvas);
 console.log(ctx);
 
 
-ctx.fillStyle = "orange";
+
+
+
+class Eye1 {
+      constructor() {
+            this.startX = 120;
+            this.startY = 100;
+            this.width = 200;
+            this.height = 150;
+      }
+      draw() {
+            
+
+           ctx.fillStyle = "orange";
       
-ctx.fillRect(120, 100, 200, 150);
-ctx.lineWidth = 10;
-ctx.strokeStyle = "pink";
-ctx.strokeRect(120, 100, 200, 150);
-   
+           ctx.fillRect(this.startX, this.startY, this.width, this-height);
+           ctx.lineWidth = 10;
+           ctx.strokeStyle = "pink";
+           ctx.stokeRect(this.startX, this.startY, this.width, this-height);
+      }
+}     
 
-ctx.fillStyle = "orange";
-ctx.fillRect(400, 100, 200, 150);
-ctx.lineWidth = 10;
-ctx.strokeStyle = "pink";
-ctx.strokeRect(400, 100, 200, 150);
-
-
-
-ctx.beginPath();
-ctx.moveTo(350, 350);
-ctx.lineTo(400, 700);
-ctx.lineTo(300, 700);
-ctx.lineTo(350, 350);
-ctx.strokeStyle = "hsl(320, 100%, 50%)";
-ctx.stroke();
+//ctx.fillStyle = "orange";
+//ctx.fillRect(400, 100, 200, 150);
+//ctx.lineWidth = 10;
+//ctx.strokeStyle = "pink";
+//ctx.strokeRect(400, 100, 200, 150);
 
 
+
+//ctx.beginPath();
+//ctx.moveTo(350, 350);
+//ctx.lineTo(400, 700);
+//ctx.lineTo(300, 700);
+//ctx.lineTo(350, 350);
+//ctx.strokeStyle = "hsl(320, 100%, 50%)";
+//ctx.stroke();
+
+const rect1 = new eye1();
+rect1.draw();       
    
